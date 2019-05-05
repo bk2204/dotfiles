b5411dd160482f9f805ef14333f2817005a77bed
brian m. carlson <sandals@crustytoothpaste.net>
2019-05-05 22:51:01 +0000

    misc: remove rules for removed fbtermrc
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- misc/rules.mk
+++ misc/rules.mk
@@ -1,5 +1,4 @@
 INSTALL_PAIRS	+= misc/dircolors	.dircolors
-INSTALL_PAIRS	+= misc/fbtermrc	.fbtermrc
 INSTALL_PAIRS	+= misc/libao		.libao
 INSTALL_PAIRS	+= misc/mailcap		.mailcap
 INSTALL_PAIRS	+= misc/Xsession	.Xsession
