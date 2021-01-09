9b9faaa984547233f6c5bf1771acb023fc7e1a7e
brian m. carlson <sandals@crustytoothpaste.net>
2021-01-09 18:44:33 +0000

    misc/rules.mk: create a directory for liblastfm
    As mentioned in the comments, this directory is required for scrobbling
    to work properly in Clementine.  Hopefully in the future this directory
    is created automatically and we can remove this rule, but until that
    point, let's create it so it exists.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- misc/rules.mk
+++ misc/rules.mk
@@ -3,6 +3,9 @@ INSTALL_PAIRS	+= misc/libao		.libao
 INSTALL_PAIRS	+= misc/mailcap		.mailcap
 INSTALL_PAIRS	+= misc/Xsession	.Xsession
 
+# If this is missing, Clementine using last.fm won't scrobble.
+INSTALL_DIRS	+= .local/share/Last.fm
+
 install: install-misc
 
 install-misc:
