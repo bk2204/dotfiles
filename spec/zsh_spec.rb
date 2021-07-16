ad1ab7ed5f4d12289a4294ea1ad350779458d26c
brian m. carlson <sandals@crustytoothpaste.net>
2021-07-16 01:10:13 +0000

    zsh/zshenv: prefer Neovim over Vim
    Neovim has support for some additional features, so let's use it in
    favor of Vim.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/zsh_spec.rb
+++ spec/zsh_spec.rb
@@ -6,20 +6,20 @@ describe :zsh do
   end
 
   context 'editor' do
-    it 'should set EDITOR to gvim with DISPLAY' do
-      expect(@dir.cmd(['zsh', '-c', 'echo $EDITOR'], 'DISPLAY' => 'something')).to eq "gvim -f\n"
+    it 'should set EDITOR to nvim-gtk with DISPLAY' do
+      expect(@dir.cmd(['zsh', '-c', 'echo $EDITOR'], 'DISPLAY' => 'something')).to eq "nvim-gtk --no-fork\n"
     end
 
     it 'should set VISUAL to gvim with DISPLAY' do
-      expect(@dir.cmd(['zsh', '-c', 'echo $VISUAL'], 'DISPLAY' => 'something')).to eq "gvim -f\n"
+      expect(@dir.cmd(['zsh', '-c', 'echo $VISUAL'], 'DISPLAY' => 'something')).to eq "nvim-gtk --no-fork\n"
     end
 
     it 'should set EDITOR to ex with non-empty TERM' do
       expect(@dir.cmd(['zsh', '-c', 'echo $EDITOR'], 'TERM' => 'xterm-256color')).to eq "ex\n"
     end
 
-    it 'should set VISUAL to vim with non-empty TERM' do
-      expect(@dir.cmd(['zsh', '-c', 'echo $VISUAL'], 'TERM' => 'xterm-256color')).to eq "vim\n"
+    it 'should set VISUAL to nvim with non-empty TERM' do
+      expect(@dir.cmd(['zsh', '-c', 'echo $VISUAL'], 'TERM' => 'xterm-256color')).to eq "nvim\n"
     end
 
     it 'should set EDITOR to ex with SSH session and terminal multiplexor' do
@@ -33,7 +33,7 @@ describe :zsh do
       expect(@dir.cmd(['zsh', '-c', 'source .zshrc; echo $VISUAL'],
                       'DISPLAY' => 'something',
                       'SSH_TTY' => 'tty',
-                      'TERM' => 'screen-256color')).to eq "vim\n"
+                      'TERM' => 'screen-256color')).to eq "nvim\n"
     end
 
     it 'should set EDITOR to ex with TERM=dumb' do
