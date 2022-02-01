791bd6cc5a318274ad7c2bb61e64afb26740fd20
brian m. carlson <sandals@crustytoothpaste.net>
2022-02-01 22:11:22 +0000

    zsh: alias gvi to a detachable editor
    We'd like to provide for an alias to use the best detachable editor
    possible.  Let's ask bmc_editor to provide such an option and alias it
    to "gvi".
    
    If there isn't one, let's not create the alias.  To do that, we want to
    choose whether we want to fall back to vi (in this case, we do not) and
    avoid creating it in that case.  In order to make this easier, add
    --fallback and --no-fallback options to bmc_editor to decide whether a
    fallback is desired.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/zsh_spec.rb
+++ spec/zsh_spec.rb
@@ -21,13 +21,13 @@ describe :zsh do
     it 'should set set detachable editor to nvim-gtk with DISPLAY' do
       @dir = TestDir.new
       exes = %w[nvim-gtk gvim mvim ex nvim vimx vim vi]
-      expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'bmc_editor --detach'], 'DISPLAY' => 'something')).to eq "nvim-gtk"
+      expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'bmc_editor --detach --no-fallback'], 'DISPLAY' => 'something')).to eq "nvim-gtk"
     end
 
     it 'should prefer gvim with DISPLAY and no nvim-gtk' do
       @dir = TestDir.new
       exes = %w[gvim mvim ex nvim vimx vim vi]
-      expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'echo $VISUAL; echo $EDITOR; bmc_editor --detach'], 'DISPLAY' => 'something')).to eq "gvim -f\ngvim -f\ngvim"
+      expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'echo $VISUAL; echo $EDITOR; bmc_editor --detach --no-fallback'], 'DISPLAY' => 'something')).to eq "gvim -f\ngvim -f\ngvim"
     end
 
     it 'should set EDITOR to ex with non-empty TERM' do
@@ -38,6 +38,12 @@ describe :zsh do
       @dir = TestDir.new
       exes = %w[nvim-gtk gvim mvim ex nvim vimx vim vi]
       expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'echo $VISUAL'], 'TERM' => 'xterm-256color')).to eq "nvim\n"
+    end
+
+    it 'should not pick a detachable editor with no display' do
+      @dir = TestDir.new
+      exes = %w[nvim-gtk gvim ex nvim vimx vim vi]
+      expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'bmc_editor --detach --no-fallback'], 'TERM' => 'xterm-256color')).to eq ""
     end
 
     it 'should set VISUAL to nvim with DISPLAY, non-empty TERM, and no graphical editors' do
