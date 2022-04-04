51de0a0bb0dff5202201f6a0aaf59d6a893cf719
brian m. carlson <sandals@crustytoothpaste.net>
2022-04-04 21:22:09 +0000

    Add configuration for Lawn
    Add configuration for Lawn itself plus a wrapper to properly invoke
    Neovim over SSH either on a remote system or in Codespaces.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- misc/rules.mk
+++ misc/rules.mk
@@ -2,9 +2,10 @@ INSTALL_PAIRS	+= misc/dircolors	.dircolors
 INSTALL_PAIRS	+= misc/libao		.libao
 INSTALL_PAIRS	+= misc/mailcap		.mailcap
 INSTALL_PAIRS	+= misc/Xsession	.Xsession
+INSTALL_PAIRS	+= misc/lawn.yaml	.config/lawn/config.yaml
 
 # If this is missing, Clementine using last.fm won't scrobble.
-INSTALL_DIRS	+= .local/share/Last.fm
+INSTALL_DIRS	+= .local/share/Last.fm .config/lawn
 
 install: install-misc
 
