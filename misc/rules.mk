e027c80b16b6bddcf17290cbce93fbf07b27a648
brian m. carlson <sandals@crustytoothpaste.net>
2023-04-25 00:56:07 +0000

    Add an Alacritty config file
    Alacritty provides some nice features over MATE Terminal, including easy
    configuration with a configuration file.  Add such a configuration file
    and update the license accordingly.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- misc/rules.mk
+++ misc/rules.mk
@@ -3,9 +3,10 @@ INSTALL_PAIRS	+= misc/libao		.libao
 INSTALL_PAIRS	+= misc/mailcap		.mailcap
 INSTALL_PAIRS	+= misc/Xsession	.Xsession
 INSTALL_PAIRS	+= misc/lawn.yaml	.config/lawn/config.yaml
+INSTALL_PAIRS	+= misc/alacritty.yml	.config/alacritty/alacritty.yml
 
 # If this is missing, Clementine using last.fm won't scrobble.
-INSTALL_DIRS	+= .local/share/Last.fm .config/lawn
+INSTALL_DIRS	+= .local/share/Last.fm .config/lawn .config/alacritty
 
 install: install-misc
 
