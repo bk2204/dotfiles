8d7d5228e1738e37a1bb4da2e277224069557257
brian m. carlson <sandals@crustytoothpaste.net>
2023-07-06 21:07:29 +0000

    zsh: make zprofile load zshenv
    In our configuration, zshenv loads all of the environment variables we
    intend to use generally, including PATH.  However, on some systems, the
    system profile code overrides PATH, leaving us with a broken PATH that
    doesn't include anything we've set.  Notably, this occurs on a Codespace
    if PATH is set by the Codespace.
    
    To solve this, have zprofile, which runs after the system profile code,
    simply source zshenv, which will stuff the overridden PATH code at the
    end instead.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- zsh/rules.mk
+++ zsh/rules.mk
@@ -1,4 +1,5 @@
 INSTALL_PAIRS	+= zsh/zshenv     .zshenv
 INSTALL_PAIRS	+= zsh/zshrc      .zshrc
 INSTALL_PAIRS	+= zsh/zlogout    .zlogout
+INSTALL_PAIRS	+= zsh/zprofile   .zprofile
 INSTALL_PAIRS	+= zsh/functions  .zsh
