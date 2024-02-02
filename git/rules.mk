60e0030e1efb911da53e23c2a8afb922930b353e
brian m. carlson <sandals@crustytoothpaste.net>
2024-02-02 12:59:33 +0000

    git: add a templated `~/.config/git/config` file
    It's useful to have some per-environment settings in Git, such as
    whether commit signing is enabled and which credential helper to use.
    Read this data from the configuration file and generate a secondary
    Git config file in the XDG-compatible location.
    
    If the credential helper is specified as `os`, then pick the right one
    for the OS that we're on.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- git/rules.mk
+++ git/rules.mk
@@ -1,1 +1,7 @@
 INSTALL_PAIRS	+= git/gitconfig .gitconfig
+INSTALL_DIRS	+= .config/git
+
+ifeq ($(TEMPLATE),1)
+TEMPLATE_FILES	+= git/xdg-git-config.gen
+INSTALL_PAIRS	+= git/xdg-git-config.gen .config/git/config
+endif
