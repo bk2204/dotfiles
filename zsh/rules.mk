86b0812105a56c76f077364445d91599bdba4694
brian m. carlson <sandals@crustytoothpaste.net>
2024-02-14 03:02:19 +0000

    zsh: use template to build email and name
    Add a template to include local zshenv configuration, including personal
    name and email address.  Include the templated file into the zshenv file
    if it exists.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- zsh/rules.mk
+++ zsh/rules.mk
@@ -1,5 +1,12 @@
+INSTALL_DIRS    += .config/zsh
+
 INSTALL_PAIRS	+= zsh/zshenv     .zshenv
 INSTALL_PAIRS	+= zsh/zshrc      .zshrc
 INSTALL_PAIRS	+= zsh/zlogout    .zlogout
 INSTALL_PAIRS	+= zsh/zprofile   .zprofile
 INSTALL_PAIRS	+= zsh/functions  .zsh
+
+ifeq ($(TEMPLATE),1)
+TEMPLATE_FILES	+= zsh/zshenvlocal.gen
+INSTALL_PAIRS	+= zsh/zshenvlocal.gen .config/zsh/zshenvlocal
+endif
