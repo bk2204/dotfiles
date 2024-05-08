837338078aa5c50cd4b2404220abddd0ef939a81
brian m. carlson <bk2204@github.com>
2024-05-08 18:04:45 +0000

    Neuter Azure CLI surveys and telemetry
    The Azure CLI surveys and telemetry should already have been fixed by
    the appropriate configuration file that we've installed.  However, we
    want to be absolutely sure that any bug in the tool that might cause it
    to accidentally send surveys or telemetry will cause it to fail.
    
    To do so, symlink these files to `/dev/null`, which will hopefully make
    the tool puke or get bad data if it tries to invoke the forbidden
    functionality.
    
    Signed-off-by: brian m. carlson <bk2204@github.com>

--- misc/rules.mk
+++ misc/rules.mk
@@ -11,6 +11,10 @@ INSTALL_PAIRS	+= misc/azure-config	.azure/config
 # If this is missing, Clementine using last.fm won't scrobble.
 INSTALL_DIRS	+= .local/share/Last.fm .config/lawn .config/alacritty .azure
 
+LINK_PAIRS		+= /dev/null .azure/az_survey.json
+LINK_PAIRS		+= /dev/null .azure/telemetry
+LINK_PAIRS		+= /dev/null .azure/telemetry.txt
+
 MTREE_SOURCES	+= misc/rules-first.mtree misc/rules-main.mtree
 
 install:
