68be18eaeb52249a4bdecc8759565bc96a683b7d
brian m. carlson <sandals@crustytoothpaste.net>
2024-10-31 00:13:53 +0000

    firefox: always enable privacy headers
    We always want to indicate that we don't want to be tracked and don't
    want our information shared or sold, so enable the options which tell
    Firefox to send the appropriate headers.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- firefox/user.js
+++ firefox/user.js
@@ -1,2 +1,7 @@
 // Don't enable advertising, private or not.
 user_pref("dom.private-attribution.submission.enabled", false);
+// Always send a do-not-track header and indicate that we don't want to be
+// tracked.
+user_pref("privacy.donottrackheader.enabled", true);
+// Always indicate that we don't want our information shared or sold.
+user_pref("privacy.globalprivacycontrol.enabled", true);
