60db8b848b020f60ee6dc056017523465d03de12
brian m. carlson <sandals@crustytoothpaste.net>
2025-02-16 21:12:53 +0000

    git: allow using gpg-sq
    Sequoia PGP is a new Rust-based implementation of OpenPGP.  Unlike
    GnuPG, it plans to support v6 keys, which have improved security.  In
    addition to the standard interface, it provides a chameleon which can
    emulate GnuPG called `gpg-sq`, so allow using that in our Git
    configuration if requested.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/git_spec.rb
+++ spec/git_spec.rb
@@ -32,6 +32,37 @@ describe :git do
           helper = "libsecret"
       [gpg]
           format = openpgp
+          program = gpg
+      [user]
+          signingkey = "6A09E667BB67AE853C6EF372A54FF53A510E527F!"
+      EOM
+      expect(actual).to eq expected
+    end
+
+    it 'should generate expected values for OpenPGP signing with gpg-sq' do
+      # Fake OpenPGP fingerprint is created from the SHA-256 initial values and
+      # the SSH key is GitHub's.
+      yaml = <<~EOM
+      ---
+      git:
+          credential-helper: libsecret
+          sign-commits: gpg-sq
+      gpg:
+          key: 6A09E667BB67AE853C6EF372A54FF53A510E527F
+      ssh:
+          key: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl"
+      EOM
+      @dir = TestDir.new(config_yaml: yaml)
+      gitconfig = File.join(@dir.tempdir, ".config", "git", "config")
+      actual = File.read(gitconfig)
+      expected = <<~EOM
+      [commit]
+          gpgsign = true
+      [credential]
+          helper = "libsecret"
+      [gpg]
+          format = openpgp
+          program = gpg-sq
       [user]
           signingkey = "6A09E667BB67AE853C6EF372A54FF53A510E527F!"
       EOM
