3e91972e3b63581973860409c5b29efbf25ca991
brian m. carlson <sandals@crustytoothpaste.net>
2025-02-28 01:27:42 +0000

    zsh: install into .config/zsh
    Rather than littering the home directory with myriad files, it's nicer
    to place the configuration files under `.config`.  Do this with zsh
    files, placing them under `.config/zsh`, with functions in
    `.config/zsh/functions`, and create symlinks as necessary.  Update
    zshenv with the appropriate configuration.
    
    Remove the legacy install rules because they cannot successfully
    overwrite the old files.  The mtree-based rules are more generally
    useful anyway.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- zsh/rules.mk
+++ zsh/rules.mk
@@ -1,16 +1,7 @@
-INSTALL_DIRS    += .config/zsh
-
-INSTALL_PAIRS	+= zsh/zshenv     .zshenv
-INSTALL_PAIRS	+= zsh/zshrc      .zshrc
-INSTALL_PAIRS	+= zsh/zlogout    .zlogout
-INSTALL_PAIRS	+= zsh/zprofile   .zprofile
-INSTALL_PAIRS	+= zsh/functions  .zsh
-
 MTREE_SOURCES	+= zsh/rules-first.mtree zsh/rules-main.mtree
 
 ifeq ($(TEMPLATE),1)
 TEMPLATE_FILES	+= zsh/zshenvlocal.gen
-INSTALL_PAIRS	+= zsh/zshenvlocal.gen .config/zsh/zshenvlocal
 
 MTREE_SOURCES	+= zsh/rules-template.mtree
 endif
