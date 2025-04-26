e34292853db9e988ce842e39886fefd61af118a3
brian m. carlson <sandals@crustytoothpaste.net>
2025-04-26 18:03:38 +0000

    mutt: move to XDG standard directories
    Move the configuration to `~/.config/mutt`.  Note that the muttrc has
    been automatically loaded from `~/.config/mutt/muttrc` for 8 years, so
    this is well supported across versions.
    
    In addition, move the cache locations to `~/.cache/mutt`.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- mutt/rules.mk
+++ mutt/rules.mk
@@ -1,14 +1,14 @@
 install: install-mutt
 
-MUTT_DIRS=.mutt .mutt/header_cache .mutt/message_cache
+MUTT_DIRS=.config/mutt .cache/mutt .cache/mutt/header .cache/mutt/message
 MUTT_DIRS_FQ=$(patsubst %,$(DESTDIR)/%,$(MUTT_DIRS))
 
 $(MUTT_DIRS_FQ):
 	mkdir -p "$@"
 
 install-mutt: $(MUTT_DIRS_FQ)
-	touch "$(DESTDIR)/.mutt/aliases"
+	touch "$(DESTDIR)/.config/mutt/aliases"
 
-INSTALL_PAIRS	+= mutt/muttrc .muttrc
-INSTALL_PAIRS	+= mutt/config .mutt/config
+INSTALL_PAIRS	+= mutt/muttrc .config/muttrc
+INSTALL_PAIRS	+= mutt/config .config/mutt/config.d
 MTREE_SOURCES	+= mutt/rules-first.mtree mutt/rules-main.mtree
