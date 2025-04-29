a612ac6b2bbb71417c79b6a4a051fa29a1c61fcf
brian m. carlson <sandals@crustytoothpaste.net>
2025-04-29 22:49:57 +0000

    git: always use the XDG config path
    We have two possible cases to consider when installing a git config
    file: the template case and the fallback (non-template) case.  In the
    template case, we want the standard file, except that we also want to
    add some templated options.  In the fallback case, we only can provide
    the standard file.
    
    Traditionally, we've always installed the standard file in `.gitconfig`
    and then optionally installed the templated config in
    `.config/git/config`.  However, we'd like to eliminate the non-XDG path,
    so we'll only install the `.config/git/config` path.
    
    Preserve the standard file in its location and install it with a special
    set of mtree rules if we're not in template mode.  Otherwise, just
    extend the template to include the standard file at the end, which means
    we now only need one file: the XDG one.
    
    Be sure to add an appropriate dependency on the source file so that the
    generated file is regenerated if the source file changes.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- git/rules.mk
+++ git/rules.mk
@@ -1,9 +1,13 @@
-INSTALL_PAIRS	+= git/gitconfig .gitconfig
 INSTALL_DIRS	+= .config/git
 MTREE_SOURCES	+= git/rules-first.mtree git/rules-main.mtree
+
+git/xdg-git-config.gen: git/gitconfig
 
 ifeq ($(TEMPLATE),1)
 TEMPLATE_FILES	+= git/xdg-git-config.gen
 INSTALL_PAIRS	+= git/xdg-git-config.gen .config/git/config
 MTREE_SOURCES	+= git/rules-template.mtree
+else
+INSTALL_PAIRS	+= git/gitconfig .config/git/config
+MTREE_SOURCES	+= git/rules-fallback.mtree
 endif
