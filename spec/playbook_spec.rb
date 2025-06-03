7f6399265dd98f1b75cc9e1aad883fae11e07a5f
brian m. carlson <sandals@crustytoothpaste.net>
2025-06-03 01:55:22 +0000

    spec/playbook: test boostrapping a graphical environment
    We want to make sure that our graphical and development environment
    bootstrapping code is adequately tested.  Add a test for this case and
    adjust the container creation code to operate before each test.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/playbook_spec.rb
+++ spec/playbook_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 describe :playbooks do
   context 'docker' do
-    before(:all) do
+    before(:each) do
       skip "No docker image set" unless TestConfig.docker_tests?
       @img = TestDockerImage.new(TestConfig.docker_image)
     end
@@ -13,6 +13,15 @@ describe :playbooks do
       expect(@img.exec("DEBIAN_FRONTEND=noninteractive apt-get update")).to be true
       expect(@img.exec("DEBIAN_FRONTEND=noninteractive apt-get install -y git gnupg ansible")).to be true
       expect(@img.exec("ansible-playbook -i localhost, --connection=local playbooks/bootstrap.yaml")).to be true
+      expect(@img.exec("ansible-playbook -i localhost, --connection=local playbooks/deploy-dotfiles.yaml")).to be true
+    end
+
+    it 'should bootstrap and deploy dotfiles correctly in a graphical development environment' do
+      @img.run
+      expect(@img.setup).to be true
+      expect(@img.exec("DEBIAN_FRONTEND=noninteractive apt-get update")).to be true
+      expect(@img.exec("DEBIAN_FRONTEND=noninteractive apt-get install -y git gnupg ansible")).to be true
+      expect(@img.exec("ansible-playbook -i localhost, --connection=local -e '{\"development\":true,\"graphical\":true}' playbooks/bootstrap.yaml")).to be true
       expect(@img.exec("ansible-playbook -i localhost, --connection=local playbooks/deploy-dotfiles.yaml")).to be true
     end
   end
