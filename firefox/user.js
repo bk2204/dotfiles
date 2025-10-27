1abba8ce2b3a1827b892eb3869e50758386d1cba
brian m. carlson <sandals@crustytoothpaste.net>
2025-10-27 21:19:06 +0000

    firefox: disable various other machine learning functionality
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- firefox/user.js
+++ firefox/user.js
@@ -7,5 +7,11 @@ user_pref("privacy.donottrackheader.enabled", true);
 user_pref("privacy.globalprivacycontrol.enabled", true);
 // Turn off all machine learning functionality.
 user_pref("browser.ml.enable", false);
+user_pref("browser.ml.chat.enabled", false);
+user_pref("browser.ml.linkPreview.enabled", false);
+user_pref("browser.tabs.groups.smart.enabled", false);
+user_pref("browser.tabs.groups.smart.userEnabled", false);
+user_pref("extensions.ml.enabled", false);
+user_pref("sidebar.revamp", false);
 // Always show the full URL in the address bar.
 user_pref("browser.urlbar.trimURLs", false);
