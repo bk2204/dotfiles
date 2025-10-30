5807c41331764ce860f7a0747140de86f8598fba
brian m. carlson <sandals@crustytoothpaste.net>
2025-10-30 22:39:16 +0000

    spec: fix failing git spec
    We adjusted the installed file to always contain the full default file,
    but did not update the tests.  Update them now.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/git_spec.rb
+++ spec/git_spec.rb
@@ -2,11 +2,20 @@ require_relative 'spec_helper'
 
 describe :git do
   context 'gitconfig' do
+    def main_config
+      return @main_config if @main_config
+      @main_config = File.read(File.join(@dir.tempdir, 'src', 'git', 'gitconfig'))
+    end
+
+    def complete_config(val)
+      val + main_config
+    end
+
     it 'should generate expected values for config file with no values' do
       @dir = TestDir.new(config_yaml: "---\n{}\n")
       gitconfig = File.join(@dir.tempdir, ".config", "git", "config")
       actual = File.read(gitconfig)
-      expect(actual).to eq ""
+      expect(actual).to eq complete_config("")
     end
 
     it 'should generate expected values for OpenPGP signing and libsecret' do
@@ -36,7 +45,7 @@ describe :git do
       [user]
           signingkey = "6A09E667BB67AE853C6EF372A54FF53A510E527F!"
       EOM
-      expect(actual).to eq expected
+      expect(actual).to eq complete_config(expected)
     end
 
     it 'should generate expected values for OpenPGP signing with gpg-sq' do
@@ -66,7 +75,7 @@ describe :git do
       [user]
           signingkey = "6A09E667BB67AE853C6EF372A54FF53A510E527F!"
       EOM
-      expect(actual).to eq expected
+      expect(actual).to eq complete_config(expected)
     end
 
     it 'should generate expected values for SSH signing and osxkeychain' do
@@ -93,7 +102,7 @@ describe :git do
       [user]
           signingkey = "key::ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl"
       EOM
-      expect(actual).to eq expected
+      expect(actual).to eq complete_config(expected)
     end
   end
 end
