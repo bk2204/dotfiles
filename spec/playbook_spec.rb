c60119383a2c73a4997f062b561eab7ba5d4cb42
brian m. carlson <sandals@crustytoothpaste.net>
2025-11-08 16:10:18 +0000

    spec/playbook: test deploying dotfiles without bootstrapping
    We want to be sure that the deployment works without having Ruby
    installed so we can test the shell-based version of `dct-mtree`.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/playbook_spec.rb
+++ spec/playbook_spec.rb
@@ -24,5 +24,13 @@ describe :playbooks do
       expect(@img.exec("ansible-playbook -i localhost, --connection=local -e '{\"development\":true,\"graphical\":true}' playbooks/bootstrap.yaml")).to be true
       expect(@img.exec("ansible-playbook -i localhost, --connection=local -e '{\"private_dotfiles\":false}' playbooks/deploy-dotfiles.yaml")).to be true
     end
+
+    it 'should deploy dotfiles correctly without bootstrapping' do
+      @img.run
+      expect(@img.setup).to be true
+      expect(@img.exec("DEBIAN_FRONTEND=noninteractive apt-get update")).to be true
+      expect(@img.exec("DEBIAN_FRONTEND=noninteractive apt-get install -y git make gnupg ansible")).to be true
+      expect(@img.exec("ansible-playbook -i localhost, --connection=local -e '{\"private_dotfiles\":false}' playbooks/deploy-dotfiles.yaml")).to be true
+    end
   end
 end
