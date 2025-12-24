fd86f50e63ff2967843ee74a6cb91ffe24772b3c
brian m. carlson <sandals@crustytoothpaste.net>
2025-12-24 01:19:56 +0000

    firefox: create intermediate directories
    If we have a Firefox profile, then the directories must already exist,
    but mtree wants us to specify all parent directories before specifying
    files or subdirectories, so to keep our functionality working with
    mtree(1), specify the parent directories.
    
    To avoid errors made by omitting the second `>` before `$@`, always use
    it and simply truncate the file up front.  Note that a heredoc is not
    handled properly by make (since it wants backslashes as continuation
    lines but then emits the backslashes into the text), so we use several
    echo statements for tidiness.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- firefox/rules.mk
+++ firefox/rules.mk
@@ -6,5 +6,9 @@ INSTALL_PAIRS	+= firefox/user.js $(FIREFOX_PROFILE_DIR)/user.js
 MTREE_SOURCES	+= firefox/rules-main.mtree
 
 firefox/rules-main.mtree: $(DESTDIR)/.mozilla/firefox/profiles.ini $(DESTDIR)
-	echo "$(FIREFOX_PROFILE_DIR)/user.js type=file mode=0600 src=firefox/user.js" >$@
+	: >$@
+	echo ".mozilla type=dir mode=0700" >>$@
+	echo ".mozilla/firefox type=dir mode=0700" >>$@
+	echo "$(FIREFOX_PROFILE_DIR) type=dir mode=0700 src=firefox" >>$@
+	echo "$(FIREFOX_PROFILE_DIR)/user.js type=file mode=0600 src=firefox/user.js" >>$@
 endif
