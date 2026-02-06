875e04ddb9d1f4de1a0e40e7420e8e48a4901a05
brian m. carlson <bk2204@github.com>
2026-02-06 16:57:51 +0000

    firefox/rules.mk: handle macOS paths
    Firefox places the configuration in a different location on macOS.  Add
    a check for this case and install the `user.js` file appropriately,
    escaping the path as necessary for things to work.
    
    Remove the dependency on the `profiles.ini` file because GNU make cannot
    handle paths with spaces.
    
    Signed-off-by: brian m. carlson <bk2204@github.com>

--- firefox/rules.mk
+++ firefox/rules.mk
@@ -1,16 +1,23 @@
+UNAME_S := $(shell uname -s)
+
+ifeq ($(UNAME_S),Darwin)
+FIREFOX_PROFILE		?= $(shell (grep "Default=.*\.default*" "$(DESTDIR)/Library/Application Support/Firefox/profiles.ini" | cut -d"=" -f2) 2>/dev/null)
+FIREFOX_PROFILE_DIR	?= Library/Application Support/Firefox/$(FIREFOX_PROFILE)
+else
 FIREFOX_PROFILE		?= $(shell (grep "Default=.*\.default*" "$(DESTDIR)/.mozilla/firefox/profiles.ini" | cut -d"=" -f2) 2>/dev/null)
 FIREFOX_PROFILE_DIR	?= .mozilla/firefox/$(FIREFOX_PROFILE)
+endif
 
 ifneq ($(FIREFOX_PROFILE),)
 INSTALL_PAIRS	+= firefox/user.js $(FIREFOX_PROFILE_DIR)/user.js
 MTREE_SOURCES	+= firefox/rules-main.mtree
 
-firefox/rules-main.mtree: $(DESTDIR)/.mozilla/firefox/profiles.ini $(DESTDIR)
+firefox/rules-main.mtree: $(DESTDIR)
 	: >$@
 	echo ".mozilla type=dir mode=0700" >>$@
 	echo ".mozilla/firefox type=dir mode=0700" >>$@
-	echo "$(FIREFOX_PROFILE_DIR) type=dir mode=0700 src=firefox" >>$@
-	echo "$(FIREFOX_PROFILE_DIR)/user.js type=file mode=0600 src=firefox/user.js" >>$@
+	echo "$$(echo "$(FIREFOX_PROFILE_DIR)" | sed -e 's/ /\\s/g') type=dir mode=0700 src=firefox" >>$@
+	echo "$$(echo "$(FIREFOX_PROFILE_DIR)" | sed -e 's/ /\\s/g')/user.js type=file mode=0600 src=firefox/user.js" >>$@
 
 GENERATED_FILES += firefox/rules-main.mtree
 endif
