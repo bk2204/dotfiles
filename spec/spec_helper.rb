b620b6a3d4e2351685cc3ae8b8aa603c17e72c00
brian m. carlson <sandals@crustytoothpaste.net>
2026-03-01 21:36:12 +0000

    vimrc: test executable functionality in Vader
    We can use Vader to test parts of our executable manipulation
    functionality.  Expose the script-local functions with a separate helper
    and split out the main portion of the function.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -152,3 +152,14 @@ class TestDir
     end
   end
 end
+
+def status(command, **env)
+  res = nil
+  IO.popen(env, command, :in => "/dev/null", :err => [:child, :out], :chdir => File.join(File.dirname(__FILE__), "..")) do |io|
+      res = io.read
+  end
+  unless $?.success?
+      $stderr.puts res
+  end
+  $?.exitstatus
+end
