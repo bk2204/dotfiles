a4023ffd9c1cf3e658f3a01e8c88ede25333b790
brian m. carlson <sandals@crustytoothpaste.net>
2026-03-09 22:29:39 +0000

    spec/jump: use `--print` when possible
    This makes the command work better when testing on unfamiliar systems.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/jump_spec.rb
+++ spec/jump_spec.rb
@@ -15,33 +15,33 @@ describe :dct_jump do
 
   it 'should greet appropriately' do
     @dir = TestDir.new
-    expect(@dir.cmd(['bin/dct-jump', 'hello'])).to eq "Hello, world!\n"
-    expect(@dir.cmd(['bin/dct-jump', 'hello', 'Toronto'])).to eq "Hello, Toronto!\n"
-    expect(@dir.cmd(['bin/dct-jump', 'hello', 'New', 'Jersey'])).to eq "Hello, New Jersey!\n"
+    expect(@dir.cmd(['bin/dct-jump', '--print', 'hello'])).to eq "Hello, world!\n"
+    expect(@dir.cmd(['bin/dct-jump', '--print', 'hello', 'Toronto'])).to eq "Hello, Toronto!\n"
+    expect(@dir.cmd(['bin/dct-jump', '--print', 'hello', 'New', 'Jersey'])).to eq "Hello, New Jersey!\n"
   end
 
   it 'should dismiss appropriately' do
     @dir = TestDir.new
-    expect(@dir.cmd(['bin/dct-jump', 'good-bye'])).to eq "Goodbye, world!\n"
-    expect(@dir.cmd(['bin/dct-jump', 'good-bye', 'Toronto'])).to eq "Goodbye, Toronto!\n"
-    expect(@dir.cmd(['bin/dct-jump', 'good-bye', 'New', 'Jersey'])).to eq "Goodbye, New Jersey!\n"
+    expect(@dir.cmd(['bin/dct-jump', '--print', 'good-bye'])).to eq "Goodbye, world!\n"
+    expect(@dir.cmd(['bin/dct-jump', '--print', 'good-bye', 'Toronto'])).to eq "Goodbye, Toronto!\n"
+    expect(@dir.cmd(['bin/dct-jump', '--print', 'good-bye', 'New', 'Jersey'])).to eq "Goodbye, New Jersey!\n"
   end
 
   it 'should open expected URLs' do
     @dir = TestDir.new
-    expect(@dir.cmd(%w[bin/dct-jump es sin querer queriendo], **@env)).to eq "https://www.wordreference.com/esen/sin%20querer%20queriendo\n"
-    expect(@dir.cmd(%w[bin/dct-jump esfr sin querer queriendo], **@env)).to eq "https://www.wordreference.com/fres/sin%20querer%20queriendo\n"
-    expect(@dir.cmd(%w[bin/dct-jump fr bon débarras], **@env)).to eq "https://www.wordreference.com/fren/bon%20d%c3%a9barras\n"
+    expect(@dir.cmd(%w[bin/dct-jump --print es sin querer queriendo], **@env)).to eq "https://www.wordreference.com/esen/sin\\%20querer\\%20queriendo\n"
+    expect(@dir.cmd(%w[bin/dct-jump --print esfr sin querer queriendo], **@env)).to eq "https://www.wordreference.com/fres/sin\\%20querer\\%20queriendo\n"
+    expect(@dir.cmd(%w[bin/dct-jump --print fr bon débarras], **@env)).to eq "https://www.wordreference.com/fren/bon\\%20d\\%c3\\%a9barras\n"
   end
 
   it 'should open expected repo URLs' do
     @dir = TestDir.new
-    expect(@dir.cmd(%w[bin/dct-jump lfs], **@env)).to eq "https://github.com/git-lfs/git-lfs/\n"
-    expect(@dir.cmd(%w[bin/dct-jump lfs i1234], **@env)).to eq "https://github.com/git-lfs/git-lfs/issues/1234\n"
-    expect(@dir.cmd(%w[bin/dct-jump lfs p1234], **@env)).to eq "https://github.com/git-lfs/git-lfs/pull/1234\n"
-    expect(@dir.cmd(%w[bin/dct-jump lfs 1234], **@env)).to eq "https://github.com/git-lfs/git-lfs/issues/1234\n"
-    expect(@dir.cmd(%w[bin/dct-jump lfs i], **@env)).to eq "https://github.com/git-lfs/git-lfs/issues/\n"
-    expect(@dir.cmd(%w[bin/dct-jump lfs p], **@env)).to eq "https://github.com/git-lfs/git-lfs/pull/\n"
+    expect(@dir.cmd(%w[bin/dct-jump --print lfs], **@env)).to eq "https://github.com/git-lfs/git-lfs/\n"
+    expect(@dir.cmd(%w[bin/dct-jump --print lfs i1234], **@env)).to eq "https://github.com/git-lfs/git-lfs/issues/1234\n"
+    expect(@dir.cmd(%w[bin/dct-jump --print lfs p1234], **@env)).to eq "https://github.com/git-lfs/git-lfs/pull/1234\n"
+    expect(@dir.cmd(%w[bin/dct-jump --print lfs 1234], **@env)).to eq "https://github.com/git-lfs/git-lfs/issues/1234\n"
+    expect(@dir.cmd(%w[bin/dct-jump --print lfs i], **@env)).to eq "https://github.com/git-lfs/git-lfs/issues/\n"
+    expect(@dir.cmd(%w[bin/dct-jump --print lfs p], **@env)).to eq "https://github.com/git-lfs/git-lfs/pull/\n"
   end
 
   it 'should edit notes' do
@@ -49,6 +49,6 @@ describe :dct_jump do
   end
 
   it 'should print repos' do
-    expect(@dir.cmd(%w[bin/dct-jump repos], **@renv)).to match %r[^git\tgit/git$]m
+    expect(@dir.cmd(%w[bin/dct-jump --print repos], **@renv)).to match %r[^git\tgit/git$]m
   end
 end
