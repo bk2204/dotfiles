c3854fd86220bd95f1b61b678e2d2cb216cb02fa
brian m. carlson <sandals@crustytoothpaste.net>
2026-03-13 11:09:38 +0000

    vim: build spell files when available
    We speak a variety of languages: English (both U.S. and Canadian
    variants), Spanish, and French (plus a little German).  It's very
    valuable to have correct spelling dictionaries available so that we can
    write correct text in a variety of languages using appropriate
    dialectical spelling.  In addition, it is quite irksome to be told that
    "behaviour" is not a valid word when every Tom, Dick, and Harry can
    clearly see that it is.
    
    Vim and Neovim require their own format of spelling dictionary but are
    capable of building their spelling dictionaries from Myspell or Hunspell
    files (which come in pairs as `.aff` and `.dic` files).  GNU Make lacks
    appropriate primitives for matching in the appropriate way to build
    these files without hard-coding dependencies, so write a script to build
    the dependencies for GNU make as well as generate the files.
    
    Make sure that we have the `hunspell` package package installed and the
    appropriate dictionaries.  Include both U.S. and Canadian English and
    use the classical French spelling dictionary since the 1990 reform did
    violence to the circumflex accent.
    
    Mark the generated mtree files and the entire spell directory as
    autogenerated so they will be cleaned when `make clean` is run.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- vim/rules.mk
+++ vim/rules.mk
@@ -1,1 +1,36 @@
 MTREE_SOURCES	+= vim/rules-main.mtree
+
+HUNSPELL ?= $(shell command -v hunspell 2>/dev/null)
+VIMLIKE ?= $(shell command -v nvim 2>/dev/null || command -v vim 2>/dev/null)
+LANGUAGES ?= en_CA en_US es_MX es_US fr_CA fr_FR
+
+ifneq ($(HUNSPELL),)
+ifneq ($(VIMLIKE),)
+vim/spell:
+	mkdir -p $@
+
+vim/spell/spell.d: vim/spell
+	snippets/exec/build-vim-spell -d vim/spell --extra-target=vim/rules-spell.mtree -o $@ --depend $(LANGUAGES)
+
+vim/rules-spell.mtree: vim/spell/spell.d
+
+-include vim/spell/spell.d
+
+vim/spell/%.utf-8.spl:
+	snippets/exec/build-vim-spell -o $@ --restrict --build $(filter $*_%,$(LANGUAGES))
+
+vim/rules-spell.mtree:
+	: >$@
+	for i in $^; \
+	do \
+		if [ -f "$$i" ]; \
+		then \
+			[ -z "$${i##*.spl}" ] || continue; \
+			echo ".vim/spell/$$(basename $$i) type=file mode=0640 src=$$i" >>$@; \
+		fi; \
+	done
+
+MTREE_SOURCES	+= vim/rules-spell.mtree
+GENERATED_FILES += vim/rules-spell.mtree vim/spell
+endif
+endif
