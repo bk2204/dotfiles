b7517b5fa71b96f1056e524e30271338d89628c6
brian m. carlson <sandals@crustytoothpaste.net>
2026-03-14 00:26:23 +0000

    spec: run Vader tests with runtimepath
    In a container or in CI, we will not have installed our dotfiles, so the
    Vim packages we use will not be present, causing some of our vimrc code
    to fail and the tests along with it.  Fix this by reading `$VIMRUNTIME`
    from Vim or Neovim and setting the `runtimepath` variable to include
    both that path (for system functionality we require) plus our `vim`
    directory for test purposes.  Use `--cmd` for this so it gets set before
    the vimrc is loaded.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/vader_spec.rb
+++ spec/vader_spec.rb
@@ -2,12 +2,22 @@ require_relative 'spec_helper'
 
 describe :vader do
   context 'vader tests' do
+    def runtimepath(command)
+      rtp = nil
+      IO.popen([command, '-es', '+put=$VIMRUNTIME|print|quit!'], :in => "/dev/null") do |io|
+        rtp = io.read
+      end
+      rtp.rstrip
+    end
+
     it 'should pass all the Vader tests successfully correctly with Neovim' do
-      expect(status(['nvim', '-es', '-u', 'vim/vimrc', '-i', 'NONE', '-c' 'Vader! vim/vader/*.vader'], "TERM" => "dumb")).to eq 0
+      rtp = "#{runtimepath('nvim')},#{File.absolute_path("vim")}"
+      expect(status(['nvim', '-es', '-u', 'vim/vimrc', '-i', 'NONE', '--cmd', %Q[set rtp=#{rtp}], '-c' 'Vader! vim/vader/*.vader'], "TERM" => "dumb")).to eq 0
     end
 
     it 'should pass all the Vader tests successfully correctly with Vim' do
-      expect(status(['vim', '-es', '-u', 'vim/vimrc', '-i', 'NONE', '-c' 'Vader! vim/vader/*.vader'], "TERM" => "dumb")).to eq 0
+      rtp = "#{runtimepath('vim')},#{File.absolute_path("vim")}"
+      expect(status(['vim', '-es', '-u', 'vim/vimrc', '-i', 'NONE', '--cmd', %Q[set rtp=#{rtp}], '-c' 'Vader! vim/vader/*.vader'], "TERM" => "dumb")).to eq 0
     end
   end
 end
