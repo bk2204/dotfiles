2e6189401254b0ee7a5794b2780a9795db08c072
brian m. carlson <sandals@crustytoothpaste.net>
2026-03-14 19:20:02 +0000

    spec: test bootstrap of non-graphical development environments
    This is useful for focal, which is used for development on occasion, but
    cannot be bootstrapped graphically due to ancient software.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/playbook_spec.rb
+++ spec/playbook_spec.rb
@@ -16,6 +16,15 @@ describe :playbooks do
       expect(@img.exec("ansible-playbook -v -i localhost, --connection=local -e '{\"private_dotfiles\":false}' playbooks/deploy-dotfiles.yaml")).to be true
     end
 
+    it 'should bootstrap and deploy dotfiles correctly in a non-graphical development environment' do
+      @img.run
+      expect(@img.setup).to be true
+      expect(@img.exec("DEBIAN_FRONTEND=noninteractive apt-get update")).to be true
+      expect(@img.exec("DEBIAN_FRONTEND=noninteractive apt-get install -y git gnupg ansible")).to be true
+      expect(@img.exec("ansible-playbook -v -i localhost, --connection=local -e '{\"development\":true,\"graphical\":false}' playbooks/bootstrap.yaml")).to be true
+      expect(@img.exec("ansible-playbook -v -i localhost, --connection=local -e '{\"private_dotfiles\":false}' playbooks/deploy-dotfiles.yaml")).to be true
+    end
+
     it 'should bootstrap and deploy dotfiles correctly in a graphical development environment' do
       skip if ENV['DOCKER_IMAGE'] == 'ubuntu:focal'
       @img.run
