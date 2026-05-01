8d77e504412e5e8fa7b9724233720ae860f73736
brian m. carlson <sandals@crustytoothpaste.net>
2026-05-01 21:57:12 +0000

    kitty: generate font configuration from main config
    We'd like to be able to customize our font configuration in a single
    place: our main YAML config file.  Add a directory with config files for
    kitty and automatically load it, then generate a config snippet based on
    the font settings in the YAML config file.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- kitty/rules.mk
+++ kitty/rules.mk
@@ -1,1 +1,6 @@
 MTREE_SOURCES	+= kitty/rules-first.mtree kitty/rules-main.mtree
+
+ifeq ($(TEMPLATE),1)
+TEMPLATE_FILES	+= kitty/conf.d/fonts.conf.gen
+MTREE_SOURCES	+= kitty/rules-template.mtree
+endif
