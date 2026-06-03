65e5fa854150a18a41194ea7bc21c91a7c1b4347
brian m. carlson <bk2204@github.com>
2026-06-03 13:45:06 +0000

    spec: test bootstrapping a Codespace
    We'd like to make sure we can bootstrap a GitHub Codespace, so use our
    bootstrap code to run the playbooks we would use in a container.
    
    Signed-off-by: brian m. carlson <bk2204@github.com>

--- spec/playbook_spec.rb
+++ spec/playbook_spec.rb
@@ -45,6 +45,14 @@ describe :playbooks do
       expect(@img.exec("ansible-playbook -v -i localhost, --connection=local -e '{\"private_dotfiles\":false}' playbooks/deploy-dotfiles.yaml")).to be true
     end
 
+    it 'should bootstrap a Codespace correctly' do
+      @img.run
+      expect(@img.setup).to be true
+      expect(@img.exec("DEBIAN_FRONTEND=noninteractive apt-get update")).to be true
+      expect(@img.exec("DEBIAN_FRONTEND=noninteractive apt-get install -y git curl gnupg ansible")).to be true
+      expect(@img.exec("ansible-playbook -v -i localhost, --connection=local -e '{\"private_dotfiles\":false}' playbooks/codespace-setup.yaml")).to be true
+    end
+
     it 'should deploy dotfiles correctly without bootstrapping' do
       @img.run
       expect(@img.setup).to be true
