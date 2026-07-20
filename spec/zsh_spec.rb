5816111683f5ab6e411a221658b7dd6afa4406b9
brian m. carlson <sandals@crustytoothpaste.net>
2026-07-20 00:51:01 +0000

    zshenv: switch to namespaced functions with dashes
    zsh allows us to use dashes in function names instead of underscores,
    which is much nicer.  Switch to use dashes in all functions defined in
    zshenv and prefix our names to avoid conflicts.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- spec/zsh_spec.rb
+++ spec/zsh_spec.rb
@@ -27,7 +27,7 @@ describe :zsh do
     it 'should set set detachable editor to neovide with DISPLAY' do
       @dir = TestDir.new
       exes = %w[lawn neovide nvim-gtk gvim mvim ex nvim vimx vim vi]
-      expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'bmc_editor --detach --no-fallback'], 'DISPLAY' => 'something')).to eq "neovide --fork"
+      expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'dct-editor --detach --no-fallback'], 'DISPLAY' => 'something')).to eq "neovide --fork"
     end
 
     it 'should set EDITOR to lawn with REMOTE_ENV' do
@@ -45,7 +45,7 @@ describe :zsh do
     it 'should set set detachable editor to lawn with REMOTE_ENV' do
       @dir = TestDir.new
       exes = %w[lawn nvim-gtk gvim mvim ex nvim vimx vim vi]
-      expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'bmc_editor --detach --no-fallback'], 'REMOTE_ENV' => 'something')).to eq "lawn run -- gvi"
+      expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'dct-editor --detach --no-fallback'], 'REMOTE_ENV' => 'something')).to eq "lawn run -- gvi"
     end
 
     it 'should set BROWSER to lawn with REMOTE_ENV' do
@@ -57,7 +57,7 @@ describe :zsh do
     it 'should prefer gvim with DISPLAY and no nvim-gtk' do
       @dir = TestDir.new
       exes = %w[gvim mvim ex nvim vimx vim vi]
-      expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'echo $VISUAL; echo $EDITOR; bmc_editor --detach --no-fallback'], 'DISPLAY' => 'something')).to eq "gvim -f\ngvim -f\ngvim"
+      expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'echo $VISUAL; echo $EDITOR; dct-editor --detach --no-fallback'], 'DISPLAY' => 'something')).to eq "gvim -f\ngvim -f\ngvim"
     end
 
     it 'should set EDITOR to nex with non-empty TERM' do
@@ -81,7 +81,7 @@ describe :zsh do
     it 'should not pick a detachable editor with no display' do
       @dir = TestDir.new
       exes = %w[nvim-gtk gvim ex nvim vimx vim vi]
-      expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'bmc_editor --detach --no-fallback'], 'TERM' => 'xterm-256color')).to eq ""
+      expect(@dir.cmd_with_exes(exes, ['zsh', '-c', 'dct-editor --detach --no-fallback'], 'TERM' => 'xterm-256color')).to eq ""
     end
 
     it 'should set VISUAL to nvim with DISPLAY, non-empty TERM, and no graphical editors' do
